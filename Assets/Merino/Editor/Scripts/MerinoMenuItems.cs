3a9b98d7ec93a56800cc23ae9b459b98a14ab65c
Adrienne Lombardo <hello@charblar.com>
2019-03-07 16:36:13 -0800

    Added MerinoMenuItems, includes methods for exporting packages for Merino releases, add "MERINO_DEVELOPER" to the scripting define symbols to appear.

---
+++ Assets/Merino/Editor/Scripts/MerinoMenuItems.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+
+public static class MerinoMenuItems
+{
+#if MERINO_DEVELOPER
+    [MenuItem("Merino/Utilities/Export Minimal Package", priority = 0)]
+    static void ExportMinimalPackage()
+    {
+        string path = EditorUtility.SaveFilePanel("Export Package", "", "merino_0.0.0_minimal", "unitypackage");
+        if (path.Length == 0) return;
+
+        AssetDatabase.ExportPackage("Assets/Merino", path, ExportPackageOptions.Recurse);
+    }
+    
+    [MenuItem("Merino/Utilities/Export Complete Package", priority = 1)]
+    static void ExportCompletePackage()
+    {
+        string path = EditorUtility.SaveFilePanel("Export Package", "", "merino_0.0.0_complete", "unitypackage");
+        if (path.Length == 0) return;
+
+        string[] folders = new string[] {"Assets/Merino", "Assets/YarnSpinner"};
+        AssetDatabase.ExportPackage(folders, path, ExportPackageOptions.Recurse);
+    }
+#endif
+}

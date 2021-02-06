6cbaf664e1c42cf51c9e374191afef2a57592ec0
Robert Yang <2285943+radiatoryang@users.noreply.github.com>
2021-02-07 12:19:49 +1300

    moved everything to Package folder structure, but MerinoEditorResources is now broken

---
+++ Editor/Scripts/MerinoEventWindow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Merino {
+    /// <summary>
+    /// Abstract class for all Merino EditorWindows.
+    /// </summary>
+    public abstract class MerinoEventWindow : EventWindow
+    {
+        public abstract void Refresh();
+    }
+}
